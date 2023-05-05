0397f70b279a8455397eccb6e0d09a989ec2e1d7
Mary <mary@moralis.io>
2023-05-05 13:05:52 +0100

    update

--- pages/api/moralis/[...moralis].ts
+++ pages/api/moralis/[...moralis].ts
@@ -1,7 +1,7 @@
 import { MoralisNextApi } from '@moralisweb3/next';
 
 export default MoralisNextApi({
-    apiKey: process.env.MORALIS_NEW_SECRET,
+    apiKey: process.env.MORALIS_API_KEY,
     authentication: {
         domain: 'amazing.dapp',
         uri: process.env.NEXTAUTH_URL,
